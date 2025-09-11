fe182f2bb75c09a9b0dc1245fcaa2f938cfd119b
Jacob Davis <jacobamdavis@gmail.com>
2025-09-11 18:42:07 -0400

    make speakers uppercase

--- resources/views/components/schedule-item.blade.php
+++ resources/views/components/schedule-item.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 <div class="flex items-center justify-between gap-x-4 px-6 py-4">
-  <div class="text-base xs:text-lg {{ strlen($title) > 30 ? 'w-2/3' : '' }} {{ $textColor }}">{{ $title }}</div>
+  <div class="uppercase text-base xs:text-lg {{ strlen($title) > 30 ? 'w-2/3' : '' }} {{ $textColor }}">{{ $title }}</div>
   <div class="text-base xs:text-lg font-medium font-mono text-right xs:text-start xs:w-auto w-1/3 {{ $textColor }}">
     <span class="xs:text-nowrap">{{ $time }}</span>
   </div>
